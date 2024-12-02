39aacf87e77612d552758f286012b049f716ab7f
Ghanadri, Michel <michel.ghanadri@intel.com>
2024-12-02 15:33:29 +0200

    created simple edge detection method

--- image_processing/test.py
+++ image_processing/test.py
@@ -1,14 +1,20 @@
 from Image import Image
+from colors import colors
 
 image = Image('cow')
 
+#################### Basics ####################
 print(image.get_image_id())
 print(image.get_image_size())
 
 image.set_pixel(10, 20, 'black')
 print(image.get_pixel(10,20))
 
-for i, key in enumerate(colors):
-    image.set_pixel_range(i*12, 50, colors[key], h=10)
-    
+#################### Colors ####################
+for i, color in enumerate(colors):
+    image.set_pixel_range(i*12, 50, color, h=10)
+
 image.show_image()
+
+edges = image.edges()
+edges.show_image()
