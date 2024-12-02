705cd9d045f9c42c84e5e842baeb54268f17f683
Ghanadri, Michel <michel.ghanadri@intel.com>
2024-12-02 15:09:19 +0200

    added colors config file

--- image_processing/test.py
+++ image_processing/test.py
@@ -5,7 +5,10 @@ image = Image('cow')
 print(image.get_image_id())
 print(image.get_image_size())
 
-image.set_pixel(10, 20, [0,0,0])
+image.set_pixel(10, 20, 'black')
 print(image.get_pixel(10,20))
 
+for i, key in enumerate(colors):
+    image.set_pixel_range(i*12, 50, colors[key], h=10)
+    
 image.show_image()
