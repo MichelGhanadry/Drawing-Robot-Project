23eb70c2b67318a4364ec5924702b751929a8a77
Ghanadri, Michel <michel.ghanadri@intel.com>
2024-12-02 12:37:35 +0200

    added gets and sets to Image

--- image_processing/Image.py
+++ image_processing/Image.py
@@ -16,3 +16,40 @@ class Image():
 
         self.image_size = self.get_image_size()
         return
+
+  def show_image(self, blocking=True):
+        imm.imshow(self.get_image_id(), self.image)
+        if blocking:
+            imm.waitKey(0)
+            imm.destroyAllWindows()
+        return
+
+########################################### Gets & Sets ###########################################
+    
+    def get_image_size(self):
+        if self.image is None:
+            rows = 0
+            cols = 0
+        else:
+            rows = len(self.image)
+            cols = len(self.image[0])
+        return rows, cols
+
+    def get_image_id(self):
+        return f"{self.image_name}.{self.image_type}"
+
+    def get_pixel(self, i=0, j=0):
+        row = i if i >= 0 and i < self.image_size[0] else 0
+        col = j if j >= 0 and j < self.image_size[1] else 0
+        return self.image[row][col].copy()
+
+    def set_pixel(self, i=0, j=0, color=[0,0,0]):
+        if (i >= 0 and i < self.image_size[0]) and (j >= 0 and j < self.image_size[1]):
+            self.image[i][j] = color
+        return
+
+    def set_pixel_range(self, i=0, j=0, color=[0,0,0], h=10):
+        for x in range(h):
+            for y in range(h):
+                self.set_pixel(x+i, y+j, color)
+        return
